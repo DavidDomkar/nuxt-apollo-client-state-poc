cd362bdcda575a79dca23d8d1062940491db6354
David Domkář <dave.domkar@gmail.com>
2020-07-18 04:05:29 +0200

    feat: add typedefs

--- plugins/apollo.ts
+++ plugins/apollo.ts
@@ -1,3 +1,4 @@
+import gql from 'graphql-tag';
 import { Plugin } from '@nuxt/types';
 import { onGlobalSetup, provide } from 'nuxt-composition-api';
 import { DefaultApolloClient } from '@vue/apollo-composable';
@@ -6,9 +7,29 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 
+const typeDefs = gql`
+  type Mutation {
+    signIn(email: string!, password: string!): User
+  }
+
+  type Query {
+    isSignedIn: Boolean!
+  }
+
+  type Subscription {
+    userAdded(userId: string!): User!
+  }
+
+  type User {
+    id: string!
+    email: string!
+  }
+`;
+
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000/graphql' }),
   cache: new InMemoryCache(),
+  typeDefs,
 });
 
 const core: Plugin = () => {
